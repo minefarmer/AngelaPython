e10dcab430c03b9434b14f1db57555caec2efff1
minefarmer <pgoldfarm@yahoo.com>
2020-11-13 20:36:11 -0600

    y

---
+++ CourseNotes/BMI Caculator/bli.py
@@ -0,0 +1,19 @@
+# Don't change the code below
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# Don't change the code below
+
+# My code below this line
+
+bmi = round(weight / height ** 2)
+if bmi < 18.5:
+    print(f"Your bmi is {bmi}, you are underweight.")
+elif bmi < 25:
+    print(f"Your bmi is {bmi}, you have a normal weight.")
+elif bmi < 30:
+    print(f"Your bmi is {bmi}, you are overweight.")
+elif bmi < 35:
+    print(f"Your bmi is {bmi}, you are obese.")
+else:
+    print(f"Your bmi is {bmi}, you are clinically obese.")
+    
---
+++ CourseNotes/ControlflowLogic/OddEVenModulo.py
@@ -0,0 +1,22 @@
+# Don't change the code below  ---------------------------------------------------------
+
+# number = int(input("Which number do you want to check? "))
+
+# Don't change the code above  ------------------------------------------------------
+
+# My code below this line  --------------------------------------------------------------
+
+# x = 8
+
+# if x % 2 == 0:
+#     print("This is a even number")  # This is a even number
+# else:
+#     print("This is an odd number.")
+
+
+x = 7
+
+if x % 2 == 0:
+    print("This is a even number")
+else:
+    print("This is an odd number.")  # This is an odd number.
