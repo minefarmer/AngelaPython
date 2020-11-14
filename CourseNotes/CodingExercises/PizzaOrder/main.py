cf097236ffb7ab90ebea995dc951d20524b95b7c
minefarmer <pgoldfarm@yahoo.com>
2020-11-14 17:22:23 -0600

    break time

---
+++ CourseNotes/CodingExercises/PizzaOrder/main.py
@@ -0,0 +1,8 @@
+# Don't change the code below
+print("Welcome to Python Pizza Deliveries!")
+size = input("Welcome to Python Pizza do you want? S, M, L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# Don't change the code above
+
+# My code below this line
