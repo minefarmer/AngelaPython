535bd2ae55116b1fca82bc3f1ac5721c43684f93
minefarmer <pgoldfarm@yahoo.com>
2020-11-25 16:26:45 -0600

    test

--- CourseNotes/Functions/main.py
+++ CourseNotes/Functions/main.py
@@ -4,11 +4,29 @@
 
 
 #                     DEFINING fUNCTIONS
-def my_function():
-    print("Hello")
-    print("Bye")
+# def my_function():
+#     print("Hello")
+#     print("Bye")
 
 
-#                     CALLING A FUNCTION
-my_function()  # Hello
-                # Bye
+# #                     CALLING A FUNCTION
+# my_function()  # Hello
+#                 # Bye
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+
+# Draw square
+turn_left()
+move()
+turn_right()
+move()
+turn_right()
+move()
+turn_right()
