cd1aad00570dbebd9dd363f03bf78e50154bab0b
minefarmer <pgoldfarm@yahoo.com>
2020-11-26 14:41:46 -0600

    bye

---
+++ CourseNotes/WhileLoops/main.py
@@ -0,0 +1,48 @@
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+#  While loop
+
+for step in range(6):
+    jump()
+
+for step in range(6):
+    jump()
+
+number of hurdles = 6
+while number_of_hurdles > 0:
+    jump()
+    number_of_hurdles -= 1def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+
+fruits = ["Apple", "Pear", "Orange"]
+for fruit in fruits:
+    jump()
+
+
+
